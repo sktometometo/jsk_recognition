4332c0e18daf7b4f0552d23ce5748bdb287d1e5f
Koki Shinjo <sktometometo@gmail.com>
2021-02-28 22:24:32 +0900

    [jsk_perception] fix bugs in panorama_rect_array_to_boudning_box_array and rect_array_to_panorama_rect_array

--- jsk_perception/node_scripts/panorama_rect_array_to_bounding_box_array.py
+++ jsk_perception/node_scripts/panorama_rect_array_to_bounding_box_array.py
@@ -15,24 +15,26 @@ from geometry_msgs.msg import PointStamped
 
 def calcPoint( theta, phi, r ):
     return ( r * math.sin(theta) * math.cos(phi),
-             r * math.sin(theta) * math.cos(phi),
-             r * cos(phi) )
+             r * math.sin(theta) * math.sin(phi),
+             r * math.cos(theta) )
 
 class PanoramaRectArrayToBoundingBoxArray(object):
 
     def __init__(self):
 
         self._frame_fixed = rospy.get_param( '~frame_fixed', 'fixed_frame' )
-        self._dimensions = rospy.get_param( '~dimensions', '{}' )
+        self._dimensions = rospy.get_param( '~dimensions', {} )
 
         self._tf_buffer = tf2_ros.Buffer()
         self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
 
         mf_sub_class = message_filters.Subscriber( '~input_class', ClassificationResult )
-        mf_sub_panorama_rects = message_filters.Subsciber( '~input_panorama_rects', PanoramaRectArray )
+        mf_sub_panorama_rects = message_filters.Subscriber( '~input_panorama_rects', PanoramaRectArray )
         self._pub = rospy.Publisher( '~output', BoundingBoxArray, queue_size=1 )
 
-        tf = message_filters.TimeSynchronizer([mf_sub_class, mf_sub_panorama_rects], 10)
+        slop = rospy.get_param('~slop',0.1)
+
+        tf = message_filters.ApproximateTimeSynchronizer([mf_sub_class, mf_sub_panorama_rects], 10, slop)
         tf.registerCallback(self._cb)
 
     def _cb(self,
@@ -40,9 +42,9 @@ class PanoramaRectArrayToBoundingBoxArray(object):
             msg_panorama_rects):
 
         try:
-            transform_fixed_to_panorama = self._tf_buffer.lookup_transform_full(
+            transform_fixed_to_panorama = self._tf_buffer.lookup_transform(
+                    self._frame_fixed,
                     msg_panorama_rects.header.frame_id,
-                    self._frame_fixed.header.frame_id,
                     rospy.Time()
                     )
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
@@ -51,28 +53,30 @@ class PanoramaRectArrayToBoundingBoxArray(object):
 
         msg_pub = BoundingBoxArray()
         msg_pub.header.frame_id = self._frame_fixed
-        msg_pub.header.frame_id = rospy.Time.now()
+        msg_pub.header.stamp = rospy.Time.now()
 
-        for label, panorama_rect in zip(msg_class.labels, msg_panorama_rects.panorama_rects):
+        for label, label_name, panorama_rect in zip(msg_class.labels, msg_class.label_names, msg_panorama_rects.panorama_rects):
 
-            if label not in self._dimensions:
-                rospy.logwarn('height for id {} is not specified.'.format(label))
+            if label_name not in self._dimensions:
+                rospy.logwarn('height for label "{}" is not specified.'.format(label_name))
                 continue
+
 
             theta_center = panorama_rect.theta + panorama_rect.height / 2.0
             phi_center = panorama_rect.phi + panorama_rect.height / 2.0
-            distance = 2.0 * self._dimensions[label][2] * math.tan( panorama_rect.height / 2.0 )
+            distance = 2.0 * self._dimensions[label_name][2] * math.tan( panorama_rect.height / 2.0 )
 
             p = PointStamped()
             (p.point.x, p.point.y, p.point.z) = calcPoint(theta_center,phi_center,distance)
+            p_transformed = tf2_geometry_msgs.do_transform_point(p,transform_fixed_to_panorama)
 
             bbox = BoundingBox()
             bbox.header = msg_pub.header
-            bbox.pose.position = tf2_geometry_msgs.do_transform_point(p,transform_fixed_to_panorama)
+            bbox.pose.position = p_transformed.point
             bbox.pose.orientation.w = 1.0
-            bbox.dimensions.x = self._dimensions[label][0]
-            bbox.dimensions.y = self._dimensions[label][1]
-            bbox.dimensions.z = self._dimensions[label][2]
+            bbox.dimensions.x = self._dimensions[label_name][0]
+            bbox.dimensions.y = self._dimensions[label_name][1]
+            bbox.dimensions.z = self._dimensions[label_name][2]
             bbox.label = label
 
             msg_pub.boxes.append(bbox)
--- jsk_perception/node_scripts/rect_array_to_panorama_rect_array.py
+++ jsk_perception/node_scripts/rect_array_to_panorama_rect_array.py
@@ -7,6 +7,7 @@ from sensor_msgs.msg import Image
 from jsk_recognition_msgs.msg import PanoramaInfo, PanoramaRect, PanoramaRectArray
 from jsk_recognition_msgs.msg import RectArray
 
+import sys
 
 def transformPanoramaPoint(x,
                            y,
@@ -16,8 +17,8 @@ def transformPanoramaPoint(x,
                            theta_max,
                            phi_min,
                            phi_max):
-    phi = (phi_max - phi_min) / 2.0 + \
-            1.0 * (phi_max - phi_min) * (x - image_width/2.0) / image_width
+    phi = (phi_max + phi_min) / 2.0 + \
+            1.0 * (phi_max - phi_min) * (x - (image_width/2.0)) / image_width
     theta = theta_min + 1.0 * (theta_max - theta_min) * y / image_height
     return (theta, phi)
 
@@ -26,10 +27,14 @@ class RectArrayToPanoramaRectArray(object):
 
     def __init__(self):
 
-        msg_panorama_info = rospy.wait_for_message(
-            '~input_panorama_info', PanoramaInfo, 10)
-        msg_panorama_image = rospy.wait_for_message(
-            '~input_panorama_image', Image, 10)
+        try:
+            msg_panorama_image = rospy.wait_for_message(
+                '~input_panorama_image', Image, 10)
+            msg_panorama_info = rospy.wait_for_message(
+                '~input_panorama_info', PanoramaInfo, 10)
+        except (rospy.ROSException, rospy.ROSInterruptException) as e:
+            rospy.logerr('{}'.format(e))
+            sys.exit(1)
         self._frame_id_panorama = msg_panorama_info.header.frame_id
         self._theta_min = msg_panorama_info.theta_min
         self._theta_max = msg_panorama_info.theta_max
